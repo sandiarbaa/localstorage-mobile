18478920f3a94fce9185983cc140cab2e1c3daed
sandiarbaa <sandiarbaa@gmail.com>
2024-02-10 20:35:11 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <h1 className="text-2xl font-bold text-slate-700">Home Page</h1>
+    </div>
+  );
+}
